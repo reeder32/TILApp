b6ae25e0f49829ba89f3ebd33faae391a94917ee
reeder32 <nickreeder32@gmail.com>
2018-12-20 18:09:26 -0800

    "add parent/child relationship"

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -40,6 +40,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: User.self, database: .psql)
     migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
+
 	var commandConfig = CommandConfig.default()
 	commandConfig.useFluentCommands()
 	services.register(commandConfig)
